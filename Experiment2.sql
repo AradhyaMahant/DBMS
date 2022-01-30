ec7d82eac0291a88fd6ee2e4e4627b4531d2f5d8
Aradhya <aradhyamahant@gmail.com>
2022-01-30 16:44:52 +0530

    add

---
+++ Experiment2.sql
@@ -0,0 +1,101 @@
+CREATE DATABASE EXPERIMENT_2_B2;
+GO 
+USE EXPERIMENT_2_B2
+GO
+
+create table CLIENT_MASTER_1(
+CLIENTNO CHAR(6) CHECK(CLIENTNO like 'C%'),
+NAME VARCHAR(20) NOT NULL,
+ADDRESS1 VARCHAR(30),
+ADRESS2 VARCHAR(30),
+CITY VARCHAR(15),
+PINCODE INT ,
+STATE VARCHAR(15),
+BALDUE DECIMAL(10,2),
+PRIMARY KEY(CLIENTNO)
+);
+GO
+
+create table PRODUCT_MASTER_1(
+PRODUCTNO VARCHAR(6) CHECK(PRODUCTNO LIKE 'P%'),
+DESCRIPTION Varchar(15) NOT NULL,
+PROFITPERCENT Decimal(4,2) NOT NULL,
+UNIT_MEASURE Varchar(10) NOT NULL,
+QTYONHAND INT NOT NULL,
+REORDERL_VL INT NOT NULL,
+SELLPRICE DECIMAL(8,2) NOT NULL,
+COSTPRICE DECIMAL(8,2) NOT NULL,
+PRIMARY KEY(PRODUCTNO)
+)
+GO
+
+CREATE TABLE SALESMAN_MASTER_1(
+SALESMANNO Varchar(6) CHECK (SALESMANNO LIKE 'S%') NOT NULL,
+SALESMANNAME Varchar(20) NOT NULL,
+ADDRESS1 Varchar(30) NOT NULL,
+ADDRESS2 Varchar(30),
+CITY Varchar(20),
+PINCODE int,
+STATE Varchar(20),
+SALAMT DECIMAL(8,2)NOT NULL,
+TGTTOGET decimal(6,2) NOT NULL,
+YTDSALES decimal(6,2) NOT NULL,
+REMARKS Varchar(60),
+PRIMARY KEY(SALESMANNO)
+);
+GO
+
+--Q2 Reinsert the data in these two tables based upon Lab 1
+INSERT INTO CLIENT_MASTER_1(CLIENTNO,NAME,ADDRESS1,ADRESS2,CITY,PINCODE,STATE,BALDUE)
+values('C00001', 'Ivan bayross','','', 'Mumbai', 400054,'Maharashtra', 15000),
+('C00002', 'Mamta muzumdar','','', 'Madras', 780001,'Tamil nadu', 0),
+('C00003', 'Chhaya bankar','','', 'Mumbai', 400057,'Maharashtra', 5000),
+('C00004', 'Ashwini  joshi','','', 'Bangalore', 560001,'Karnataka', 0),
+('C00005', 'Hansel colaco','','', 'Mumbai', 400060,'Maharashtra',2000),
+('C00006', 'Deepak sharma','','', 'Mangalore', 560050,'Karnataka', 0);
+GO
+
+insert into PRODUCT_MASTER_1 values 
+('P00001','T-Shirt',5,'Piece',200,50,350,250),
+('P0345','Shirt',6,'Piece',150,50,500,350),
+('P06734','Cotton jeans',5,'Piece',100,20,600,450),
+('P07865','Jeans',5,'Piece',100,20,750,500),
+('P07868','Trousers',2,'Piece',150,50,850,550),
+('P07885','Pull Overs',2.5,'Piece',80,30,700,450),
+('P07965','Denim jeans',4,'Piece',100,40,350,250),
+('P07975','Lycra tops',5,'Piece',70,30,300,175),
+('P08865','Skirts',5,'Piece',75,30,450,300);
+GO
+
+insert into SALESMAN_MASTER_1 values 
+('S00001',	'Aman',	'A/14',	'Worli','Mumbai',400002,'Maharashtra',0,0,0,''),
+('S00002',	'Omkar','65',	'Nariman','Mumbai',400001,'Maharashtra',0,0,0,''),
+('S00003',	'Raj',	'P-7',	'Bandra','Mumbai',400032,'Maharashtra',0,0,0,''),
+('S00004',	'Ashish','A/5','Juhu','Mumbai',400044,'Maharashtra',0,0,0,'');
+GO
+
+--Q3 Display the contents of each table
+SELECT * FROM CLIENT_MASTER_1;
+SELECT *FROM SALESMAN_MASTER_1;
+SELECT * FROM PRODUCT_MASTER_1;
+
+--Q4 Create table AUTHOR
+
+CREATE TABLE AURTHOR(
+AUTHOR_ID VARCHAR(5) PRIMARY KEY,
+LASTNAME VARCHAR(15) NOT NULL,
+FIRSTNAME VARCHAR(15) NOT NULL,
+EMAIL VARCHAR(40),
+CITY VARCHAR(15),
+COUNTRY VARCHAR(15)
+);
+GO
+
+--Q5 Create Table BOOK
+
+CREATE TABLE BOOK(
+BOOK_ID VARCHAR(5) CHECK(BOOK_ID LIKE 'B%'),
+BOOK_TITLE CHAR NOT NULL,
+COPIES INT CHECK(COPIES>2), 
+PRIMARY KEY(BOOK_ID)
+);
